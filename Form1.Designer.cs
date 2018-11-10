08731ad7711a097fca075a59dd7830dec0667ee6
PoH98 <34578267+PoH98@users.noreply.github.com>
2018-11-11 00:19:04 +0800

    Updated Downloader

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace TS_Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Current Downloading: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(240, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0.ts";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 84);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "TS Downloader";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TS_Downloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string url;
+        static int num;
+        static bool Compiling = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.Contains("0.ts"))
+            {
+                MessageBox.Show("Please make sure your ts file is started from 0.ts");
+                return;
+            }
+            Directory.CreateDirectory("Download");
+            url = textBox1.Text.Replace("0.ts","");
+            WebClient wc = new WebClient();
+            wc.DownloadFile(url + 0 + ".ts", "Download\\" + 0 + ".ts");
+            Thread t = new Thread(Downloader);
+            Thread t1 = new Thread(Downloader);
+            Thread t2 = new Thread(Downloader);
+            t2.Start();
+            t.Start();
+            t1.Start();
+            timer1.Start();
+        }
+
+        private void Downloader()
+        {
+            WebClient wc = new WebClient();
+            while (true)
+            {
+                try
+                {
+                    wc.DownloadFile(url + num + ".ts", "Download\\"+ num + ".ts");
+                    num++;
+                }
+                catch (WebException wex)
+                {
+                    try
+                    {
+                        if (((HttpWebResponse)wex.Response).StatusCode == HttpStatusCode.NotFound)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            num++;
+                        }
+                    }
+                    catch
+                    {
+                        num++;
+                    }
+                }
+                catch
+                {
+                    num++;
+                }
+            }
+            Compiling = true;
+            File.WriteAllText("Download\\Run.bat", "copy /b *.ts joined_files.mp4\ndel Run.bat");
+            Process proc = Process.Start("Download\\Run.bat");
+            while (!proc.HasExited)
+            {
+                Thread.Sleep(1000);
+            }
+            Environment.Exit(0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!Compiling)
+            {
+                label2.Text = num + ".ts";
+            }
+            else
+            {
+                label1.Text = "Combining...";
+                label2.Text = "";
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
